e1dc81e32605ad75163c8fd2a2898760a364a41b
Peldan <peldans@gmail.com>
2020-03-15 20:09:38 +0100

    kan nu encoda gömma text i LSB

--- src/main/java/com/arvid/steganosaurus/StegController.java
+++ src/main/java/com/arvid/steganosaurus/StegController.java
@@ -9,22 +9,32 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toCollection;
+
 @Slf4j
 @RestController
 public class StegController {
-
     @PostMapping("/file")
     public String fileUpload(@RequestPart(value = "file") MultipartFile file) throws IOException {
-        final int CHUNK = 8;
-        ArrayList<Character> chars = decode(file.getBytes(), CHUNK, 1);
+        InputStream inputStream = new ByteArrayInputStream(file.getBytes());
+        BufferedImage img = ImageIO.read(inputStream);
+        WritableRaster raster = img.getRaster();
+        DataBufferByte bufferByte = (DataBufferByte) raster.getDataBuffer();
+        List<Character> chars = decode(bufferByte.getData(), 1);
         log.info("LSB 1: {}", chars.stream().map(String::valueOf).collect(Collectors.joining()));
-        chars = decode(file.getBytes(), CHUNK, 2);
+        chars = decode(bufferByte.getData(), 2);
         log.info("LSB 2: {}", chars.stream().map(String::valueOf).collect(Collectors.joining()));
         return Arrays.toString(file.getBytes());
     }
@@ -33,25 +43,50 @@ public class StegController {
         return b << -k >>> -k;
     }
 
-    public ArrayList<Character> decode(byte[] bytes, final int CHUNK_SIZE, final int K_LSB) {
-        ArrayList<Character> chars = new ArrayList<>();
+    public List<Character> decode(byte[] data, final int K_LSB) {
         List<Integer> lsbs = new ArrayList<>();
-        log.info("loopar igenom {}st bytes", bytes.length);
-        for (int i = 0; i < bytes.length; i++) {
-            int lsb = getLSB(bytes[i], K_LSB);
+        for (byte b : data) {
+            int lsb = getLSB(b, K_LSB);
             lsbs.add(lsb);
-            if (i > 0 && i % (CHUNK_SIZE-1) == 0) {
-                chars.add(toAscii(lsbs));
-                lsbs.clear();
-            }
         }
-        log.info("resultat blev {}st chars", chars.size());
-        return chars;
+        return new ArrayList<>(toAscii(lsbs));
     }
 
-    public char toAscii(List<Integer> binary) {
-        String temp = binary.stream().map(Integer::toBinaryString).map(String::valueOf).collect(Collectors.joining());
-        return (char) Integer.parseInt(temp, 2);
+    public byte[] encode(String text, byte[] data){
+        byte[] result = new byte[data.length];
+        System.arraycopy(data, 0, result, 0, data.length);
+        char[] toHide = text.toCharArray();
+        int byteNo = 0;
+        for (char c : toHide) {
+            byte curr = (byte) c;
+            char[] bits = getBits(curr);
+            for (char bit : bits) {
+                byte toInsertInto = data[byteNo];
+                int inserted = toInsertInto;
+                int lsb = getLSB(toInsertInto, 1);
+                if (lsb != bit) {
+                    inserted = (lsb & ~1) | bit;
+                }
+                result[byteNo] = (byte) inserted;
+                byteNo++;
+            }
+        }
+        return result;
+    }
+
+    public List<Character> toAscii(List<Integer> binary) {
+        ArrayList<String> strings = new ArrayList<>();
+        for (int i = 0; i < binary.size(); i += 8) {
+            List<Integer> subList = binary.subList(i, Math.min((i + 8), binary.size()));
+            if (!subList.isEmpty()) {
+                strings.add(subList.stream().map(Integer::toBinaryString).map(String::valueOf).collect(Collectors.joining()));
+            }
+        }
+        return strings.stream().map(e -> (char) Integer.parseInt(e, 2)).collect(toCollection(ArrayList::new));
+    }
+
+    public char[] getBits(byte b){
+        return String.format("%8s", Integer.toBinaryString(b)).replace(' ', '0').toCharArray();
     }
 
     @Bean
--- src/test/java/com/arvid/steganosaurus/StegControllerTest.java
+++ src/test/java/com/arvid/steganosaurus/StegControllerTest.java
@@ -1,17 +1,19 @@
 package com.arvid.steganosaurus;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class StegControllerTest {
@@ -30,35 +32,44 @@ public class StegControllerTest {
     @Test
     public void testToAscii(){
         List<Integer> integers = Arrays.asList(0,1,0,0,0,0,0,1);
-        Assertions.assertEquals('A', controller.toAscii(integers));
-    }
-
-    @Test
-    public void testExtractLetters(){
-        String[] byteStrings = {"64", "66", "67", "68", "69", "70", "71", "72"};
-        byte[] bytes = new byte[byteStrings.length];
-        int i = 0;
-        for(String byteString: byteStrings){
-            bytes[i++] = (byte) Integer.parseInt(byteString);
-        }
-        Assertions.assertEquals('*', controller.decode(bytes, 8, 1).get(0));
+        Assertions.assertEquals('A', controller.toAscii(integers).get(0));
     }
 
     @Test
     public void testExtractFromRealImage() throws IOException {
         File testFile = getTestFile();
         FileInputStream fis = new FileInputStream(testFile);
-        ArrayList<Character> chars = controller.decode(fis.readAllBytes(), 8, 1);
+        WritableRaster raster = ImageIO.read(fis).getRaster();
+        DataBufferByte bufferByte = (DataBufferByte) raster.getDataBuffer();
+        List<Character> chars = controller.decode(bufferByte.getData(), 1);
         String result = chars.stream().collect(Collector.of(StringBuilder::new,
                 StringBuilder::append,
                 StringBuilder::append,
                 StringBuilder::toString));
         log.info("Result {}", result);
+        log.info("b64: {}", Base64.decodeBase64(result));
         Assertions.assertTrue(result.contains("Vafan"));
     }
 
+    @Test
+    public void testEncode(){
+        Random r = new Random();
+        String alphabet = "abcdefghixyz";
+        byte[] before = new byte[24];
+        for(int i = 0; i < before.length; i++){
+            before[i] = (byte) alphabet.charAt(r.nextInt(alphabet.length()));
+        }
+        byte[] encoded = controller.encode("LOL", before);
+        Assertions.assertEquals("LOL", controller.decode(encoded, 1).stream().map(String::valueOf).collect(Collectors.joining()));
+    }
+
+    @Test
+    public void testGetBits(){
+        Assertions.assertEquals("01000001", new String(controller.getBits((byte) 'A')));
+    }
+
     private File getTestFile(){
-        String resource = "test.png";
+        String resource = "vafan.png";
         ClassLoader classLoader = getClass().getClassLoader();
         return new File(Objects.requireNonNull(classLoader.getResource(resource)).getFile());
     }
